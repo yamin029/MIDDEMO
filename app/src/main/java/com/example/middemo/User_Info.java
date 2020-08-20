f8c7e8969204e969c8cf62348506abf2ae010529
yamin <yaminkhan017@gmail.com>
2020-08-20 16:52:19 +0600

    user infor ui gradient added

--- app/src/main/java/com/example/middemo/User_Info.java
+++ app/src/main/java/com/example/middemo/User_Info.java
@@ -12,20 +12,29 @@ import java.util.ArrayList;
 public class User_Info extends AppCompatActivity {
 
     ImageView uImage;
+    ImageView bguImage;
     TextView uTvUserName;
+    TextView utvBOD;
+    TextView utvAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user__info);
-        uImage = findViewById(R.id.uImage);
-        uTvUserName = findViewById(R.id.tvUserName);
+        uImage = findViewById(R.id.ivProfile);
+        bguImage = findViewById(R.id.ivbdImage);
+        utvBOD = findViewById(R.id.tvBOD);
+        uTvUserName = findViewById(R.id.tvName);
+        utvAddress = findViewById(R.id.tvAddress);
         try {
             DatabaseHandler objectDatabaseHandler = new DatabaseHandler(this);
             ArrayList<ModelClass> objectModelClassList = objectDatabaseHandler.getUserInfo();
 
             uImage.setImageBitmap(objectModelClassList.get(0).getmImage());
+            bguImage.setImageBitmap(objectModelClassList.get(0).getmImage());
             uTvUserName.setText(objectModelClassList.get(0).getmFirstName() +" "+ objectModelClassList.get(0).getmLastName());
+            utvBOD.setText(objectModelClassList.get(0).getmDOB());
+            utvAddress.setText(objectModelClassList.get(0).getmAddress());
             //Toast.makeText(this,objectModelClassList.get(0).getmFirstName() , Toast.LENGTH_SHORT).show();
         }
         catch (Exception e){
